7c4fbddee6115c3530aec90a9680caf79ec7e112
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2016-07-07 07:12:46 +0200

    updates xsummarise_each

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,6 @@
 News for dplyrExtras
 =====================
 
+2016-07-07: Bugfix (due to new dplyr version), extension of functionality and better documentation for xsummarise_each 
+
 2016-06-28: Rename `mutate_if` to `mutate_rows`, since dplyr 5.0 introduced an own `mutate_if` function with quite different behavior. The original `mutate_if` still exists in dplyrExtras for backward compatibility, but using `mutate_rows` is preferred.
--- R/xsummarise_each.r
+++ R/xsummarise_each.r
@@ -1,81 +1,118 @@
+dots = function (...) {
+    eval(substitute(alist(...)))
+}
+named_dots = function (...) 
+{
+    args <- dots(...)
+    nms <- names(args) 
+    if (is.null(nms)) nms = rep("", length(args))
+    missing <- nms == ""
+    if (all(!missing)) 
+        return(args)
+    deparse2 <- function(x) paste(deparse(x, 500L), collapse = "")
+    defaults <- vapply(args[missing], deparse2, character(1), 
+        USE.NAMES = FALSE)
+    names(args)[missing] <- defaults
+    args
+}
  
 #' extended version of summarise_each that allows alternative arrangement of outputs
 #' 
-#' currently it is only checked whether "funs" is in .long or not. if yes
-#' then functions will be shown in different rows
-#' 
+#' @param tbl the date frame
+#' @param funs a list of functions, see the help of summarize_each, best use the helper function funs() to create them
+#' @param ... columns in tbl on which the functions should be applied
+#' @param .long either "funs" or "vars". Shall variables or functions be put in different rows?
+#' @param .wide currently ignored
+#' @.fun.var if .long = "funs" the name of the column that specifies the function
+#' @.var.var if .long = "vars" the name of the column that specifies the variable
 #' @export
-xsummarise_each = function(tbl, funs, ..., .long="funs",.wide="vars") {
-  if ("funs" %in% .long) {
+xsummarise_each = function(tbl, funs, ..., .long="funs",.wide=NA, .fun.var = ".fun", .var.var = ".var") {
+  vars = names(named_dots(...))
+  fun_names = names(funs)
+  if ("funs" %in% .long & !"vars" %in% .long) {
     i = 1
     li = lapply(seq_along(funs), function(i,...) {
       fun = as.character(funs[i][[1]])[1]
-      dt = summarise_each(tbl, funs(funs[i]), ...)
-      cbind(.fun=fun, dt)
+      dt = summarise_each(tbl, funs_(funs[i]), ...)
+      cols = (NCOL(dt)-length(vars)+1):(NCOL(dt))
+      colnames(dt)[cols] = vars
+      cbind(.fun=fun_names[i], dt)
     },...)
+    res = do.call("rbind",li)
+    colnames(res)[1] = .fun.var
+    return(res)
+
     return(do.call("rbind",li))
+  } else if ("vars" %in% .long & !"funs" %in% .long) {
+    qvars  = dots(...)
+    li = lapply(seq_along(qvars), function(i) {
+      dt = summarise_each_(tbl, funs, qvars[[i]])
+      cols = (NCOL(dt)-length(fun_names)+1):(NCOL(dt))
+      colnames(dt)[cols] = fun_names
+      cbind(.var=vars[i], dt)
+    })
+    res = do.call("rbind",li)
+    colnames(res)[1] = .var.var
+    return(res)
   }
+    
   return(summarise_each(tbl,funs,...))
 }
 
+
+
 #' extended version of summarise_each_q that allows alternative arrangement of outputs
 #' 
-#' currently it is only checked whether "funs" is in .long or not.
-#' 
+#' @param tbl the date frame
+#' @param funs a list of functions, see the help of summarize_each, best use the helper function funs() to create them
+#' @param vars columns in tbl on which the functions should be applied
+#' @param .long either "funs" or "vars". Shall variables or functions be put in different rows?
+#' @param .wide currently ignored
+#' @.fun.var if .long = "funs" the name of the column that specifies the function
+#' @.var.var if .long = "vars" the name of the column that specifies the variable
 #' @export
-xsummarise_each_q = function(tbl, funs, vars, .long="funs",.wide="vars") {
-  if ("funs" %in% .long) {
+xsummarise_each_ = xsummarise_each_q = function(tbl, funs, vars, .long="funs",.wide=NA, fun.var = ".fun", var.var = ".var") {
+  fun_names = names(funs)
+  if ("funs" %in% .long & !"vars" %in% .long) {
     i = 1
     li = lapply(seq_along(funs), function(i) {
       fun = as.character(funs[i][[1]])[1]
-      dt = summarise_each_q(tbl, funs=funs_(funs[i]), vars=vars)
-      cbind(.fun=fun, dt)
+      dt = summarise_each_(tbl, funs_(funs[i]), vars)
+      cols = (NCOL(dt)-length(vars)+1):(NCOL(dt))
+      colnames(dt)[cols] = vars
+      cbind(.fun=fun_names[i], dt)
     })
+    res = do.call("rbind",li)
+    colnames(res)[1] = .fun.var
+    return(res)
+
     return(do.call("rbind",li))
-  }
-  return(summarise_each_q(tbl,funs,vars=vars))
-}
-
-
-#' extended version of summarise_each_ that allows alternative arrangement of outputs
-#' 
-#' currently it is only checked whether "funs" is in .long or not.
-#' 
-#' @export
-xsummarise_each_ = function(tbl, funs, vars, .long="funs",.wide="vars") {
-  if ("funs" %in% .long) {
-    i = 1
-    li = lapply(seq_along(funs), function(i) {
-      fun = as.character(funs[i][[1]])[1]
-      dt = summarise_each_(tbl, funs=funs_(funs[i]), vars=vars)
-      cbind(.fun=fun, dt)
+  } else if ("vars" %in% .long & !"funs" %in% .long) {
+    li = lapply(seq_along(qvars), function(i) {
+      dt = summarise_each_(tbl, funs, vars[[i]])
+      cols = (NCOL(dt)-length(fun_names)+1):(NCOL(dt))
+      colnames(dt)[cols] = fun_names
+      cbind(.var=vars[i], dt)
     })
-    return(do.call("rbind",li))
+    res = do.call("rbind",li)
+    colnames(res)[1] = .var.var
+    return(res)
   }
+    
   return(summarise_each_(tbl,funs,vars=vars))
 }
+
 
 
 examples.xsummarise_each = function() {
   library(dplyr)
   library(dplyrExtras)
   by_species <- group_by(iris,Species)
-  by_species %>% summarise_each(funs(min, max), Petal.Width, Sepal.Width)
-  by_species %>% xsummarise_each(funs(min, max), Petal.Width, Sepal.Width)
+  summarise_each(by_species,funs(min=min, max=max, mean=mean), Petal.Width, Sepal.Width)
+  xsummarise_each(by_species,funs(min=min, max=max(.,na.rm=TRUE)), Petal.Width, Sepal.Width, .long="vars", .var.var = "Variable")
+  xsummarise_each(by_species,funs(min=min, max=max), Petal.Width, Sepal.Width, .long="funs")
   
   xsummarise_each_(by_species,funs(min, max), vars=c("Petal.Width", "Sepal.Width"))
-
-  by_species %>% xsummarise_each_(funs(min, max), vars=c("Petal.Width", "Sepal.Width"))
+  xsummarise_each_(by_species,funs(min, max), vars=c("Petal.Width", "Sepal.Width"), .long="vars", .var.var = "Variable")
 }
 
-
-examples.xsummarise_each = function(Petal.Width=NULL, Sepal.Width=NULL, Species=NULL) {
-  library(dplyr)
-  library(dplyrExtras)
-  
-  data(iris)
-  by_species <- group_by(iris,Species)
-  summarise_each(by_species,funs(min, max), Petal.Width, Sepal.Width)
-  xsummarise_each(by_species,funs(min, max), Petal.Width, Sepal.Width)
-  xsummarise_each_q(by_species,funs(min, max), vars=c("Petal.Width", "Sepal.Width"))
-}
